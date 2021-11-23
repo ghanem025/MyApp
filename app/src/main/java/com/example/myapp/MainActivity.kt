b6b9c1e24a9168041c1dcbfb400eba0ab62e2cc7
Ghanem <ghanemg@uwindsor.ca>
2021-11-22 22:27:09 -0500

    added more comments

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapp
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,37 +9,40 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var trueButton: Button // true button
+    private lateinit var falseButton: Button// false button
+    private lateinit var nextButton: Button // next button
     private lateinit var questionText: TextView
-    private val questionList = listOf(Question(R.string.question_australia,true),
+    private lateinit var alert: AlertDialog
+    private val questionList = listOf(Question(R.string.question_australia,true),// here is the list of all of our questions
         Question(R.string.math1,true),
         Question(R.string.math2,true),
         Question(R.string.math3,false))
     private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)//this will be our view
 
-        trueButton = findViewById(R.id.true_button)
+        trueButton = findViewById(R.id.true_button)//add the buttons to the view
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next)
         questionText = findViewById(R.id.questionTextView)
 
-        trueButton.setOnClickListener { view:View ->
-           checkAnswer(true)
+        trueButton.setOnClickListener { view:View ->//this will be our action listener that will perform an action when the button is clicked
+            checkAnswer(true)
+            questionUpdate()
         }
         falseButton.setOnClickListener{ view:View ->
             checkAnswer(false)
+            questionUpdate()
         }
-        nextButton.setOnClickListener {
-            index = (index+1) % questionList.size
+        nextButton.setOnClickListener {//button when clicked will update to the next question
+
             questionUpdate()
         }
 
     }
-    private fun checkAnswer(userAns:Boolean){
+    private fun checkAnswer(userAns:Boolean){// this method will perform the logic to check our answers
         val ans = questionList[index].ans
        val message = if (ans==userAns)
             R.string.correct
@@ -46,8 +50,13 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
     }
-    private fun questionUpdate(){
+    private fun questionUpdate(){//method that will be called when we want the question to be updated
+        index = (index+1) % questionList.size
         val questionTextResId = questionList[index].textResId
         questionText.setText(questionTextResId)
+        if(index==questionList.size){
+
+
+        }
     }
 }
